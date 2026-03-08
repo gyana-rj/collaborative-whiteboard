ae3f1d3f0372c152c96fb822562cd764c8c61e96
Gyana Ranjan <gyanaranjansahoo174@gmail.com>
2026-03-08 12:58:29 +0530

    Fixed the room button logic

--- apps/collaborative-board-frontend/app/components/Canvas.tsx
+++ apps/collaborative-board-frontend/app/components/Canvas.tsx
@@ -45,7 +45,7 @@ export function Canvas({roomId, socket, roomSlug}: {roomId: number; socket : Web
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
-                Room: {roomId}
+                Room: {roomSlug}
             </div>
         </div>
 }
